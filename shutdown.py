6fb5f13884b2db36403c404f930081ee46feb4f3
destiny_ <karugami02468@gmail.com>
2018-03-26 00:05:49 +0900

    時間指定のラジオボタン追加した

--- shutdown.py
+++ shutdown.py
@@ -4,10 +4,10 @@ from tkinter import ttk
 
 def function1(v1):
     if v1 == 'シャットダウン':
-        cmd = 'shutdown -s -t 3600'
+        cmd = 'shutdown -s -t '
         return subprocess.call(cmd, shell = True)
     elif v1 == '再起動':
-        cmd = 'shutdown -r -t 3600'
+        cmd = 'shutdown -r -t '
         return subprocess.call(cmd, shell = True)
     elif v1 == '中止':
         cmd = 'shutdown -a'
@@ -22,14 +22,19 @@ def show_selection():
     for i in lb.curselection():
        function1(lb.get(i))
 
+       #追加
+def show_timecommand():
+    print(v1.get())
 
+def rb_clicked():
+    show_timecommand()
 
 if __name__ == '__main__':
     root = Tk()
     root.title('shutdown timer')
     
     # シャットダウンフレーム
-    frame1 = LabelFrame(root, text="Shutdown Menu")
+    frame1 = ttk.LabelFrame(root, text="Shutdown Menu")
     frame1.grid()
 
     # シャットダウンリストボックス
@@ -44,14 +49,48 @@ if __name__ == '__main__':
 
     #↓ここからタイマー
     #タイマーフレーム
-    timerframe = LabelFrame(root, text="Timer")
+    timerframe = ttk.LabelFrame(root, text="Timer")
     timerframe.grid()
 
     #ラジオボタンで時間 分 秒 の指定
+    v1 = IntVar()
+    #時間(下の時間に*3600)
+    rb1 = ttk.Radiobutton(
+        timerframe,
+        text = '時間',
+        value = 3600,
+        variable = v1)
+    rb1.grid(row = 1,column = 0)
+
+    #分(下の時間に*60)
+    rb1 = ttk.Radiobutton(
+        timerframe, 
+        text = '  分  ',
+        value = 60,
+        variable = v1)
+    rb1.grid(row = 2,column = 0)
+
+    #秒(下の時間に*1)
+    rb1 = ttk.Radiobutton(
+        timerframe, 
+        text = '  秒  ',
+        value = 1,
+        variable = v1)
+    rb1.grid(row = 3,column = 0)
+
+    #Button
+    button2 = ttk.Button(
+        timerframe, 
+        text='OK', 
+        padding=5,
+        command=rb_clicked)
+    button2.grid(row = 2,column=1)
 
     #タイマー入力
     EditBox = Entry(timerframe,text = "時間を入力")
+    
     #EditBox.insert(END,"時間を入力")
     EditBox.grid()
     EditBox.delete(0,END)
+
     root.mainloop()
